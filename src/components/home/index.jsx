9bbd03126e413042f1a804e7b395dba0d311a64a
Gabriel Lira <103190663+GLira4k@users.noreply.github.com>
2023-05-10 00:01:53 -0300

    Adicionando botões
    Adicionei os botões de gerar link e baixar qrcode

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,26 +1,44 @@
-import { useState } from 'react'
-import QRCode from 'react-qr-code'
-import './styles.css'
+import { useState } from 'react';
+import QRCode from 'react-qr-code';
+import QRCodeLink from 'qrcode';
+import './styles.css';
 
 export default function Home() {
-  const [link, setLink] = useState(' ')
-  
-  function handleLink(e){
-    setLink(e.target.value);
+  const [linkInput, setLinkInput] = useState(' ');
+  const [link, setLink] = useState('');
+  const [qrcodeLink, setQrcodeLink] = useState('');
+
+  function handleGenerate(link_url) {
+    QRCodeLink.toDataURL(link_url, {
+      width: 600,
+      margin: 3,
+    }, function(err, url) {
+      setQrcodeLink(url);
+    });
   }
 
-  return(
+  function handleLinkInput(e) {
+    setLinkInput(e.target.value);
+  }
+
+  function handleButtonClick() {
+    setLink(linkInput);
+    handleGenerate(linkInput);
+  }
+
+  return (
     <main>
       <section>
-        <QRCode value={link}/>
+        <QRCode value={link} />
         <input
           className='inputLink'
           placeholder='Digite um link'
-          value={link}
-          onChange={(e) => handleLink(e)}
+          value={linkInput}
+          onChange={(e) => handleLinkInput(e)}
         />
+        <button onClick={handleButtonClick}>Gerar QRCode</button>
+        <button><a href={qrcodeLink} download={`QrCode`}>Baixar QRCode</a></button>
       </section>
     </main>
-  )
-
+  );
 }
